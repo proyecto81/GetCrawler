503c49118f4824787908c04f679592ca4e2bfd24
proyecto81 <proyecto81@gmail.com>
2015-03-04 18:10:00 -0300

    restart

--- descarga.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-		// Fuerzo la descarga
-		$f = $_GET['f'];
-
-		header("Content-type: application/octet-stream");
-		header("Content-Disposition: attachment; filename=\"$f\"\n");
-		//$fp = fopen("$f", "r");
-		readfile ($f);
-?>
--- template/header.php
+++
@@ -1,3 +0,0 @@
-<div class="header_cont">
-Header
-</div>
